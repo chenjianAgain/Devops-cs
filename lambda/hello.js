b1342ec1869d7c8f34b06aac130e9c8b37fb694e
joseph <joseph.siyi@gmail.com>
2019-11-27 16:34:16 +0800

    added js

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,5 +1,24 @@
+const { DynamoDB } = require('aws-sdk');
+
+
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+    console.log("Fine, thanks!");
+
+    const client = new DynamoDB.DocumentClient();
+
+    for (const record of event.Records) {
+        const body = record.body ? JSON.parse(record.body) : { };
+        await client.put({
+            TableName: process.env.TABLE_NAME,
+            Item: {
+                id: record.messageId,
+                text: body.text,
+                body
+            }
+        }).promise();
+    } 
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
--- src/CdkWorkshop/QueueRecorder.cs
+++ src/CdkWorkshop/QueueRecorder.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Amazon.CDK;
 using Amazon.CDK.AWS.SQS;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.Lambda.EventSources;
+using Amazon.CDK.AWS.DynamoDB;
 
 namespace CdkWorkshop
 {
@@ -15,14 +17,27 @@ namespace CdkWorkshop
     {
         internal QueueRecorder(Construct scope, string id, QueueRecorderProp props = null) : base(scope, id)
         {
+            var table = new Table(this, "QueueRecorderTable", new TableProps
+            {
+                PartitionKey = new Attribute
+                {
+                    Name = "id",
+                    Type = AttributeType.STRING
+                }
+            });
 
             // Defines a new lambda resource
             var hello = new Function(this, "HelloHandler", new FunctionProps
             {
                 Runtime = Runtime.NODEJS_10_X, // execution environment
                 Code = Code.FromAsset("lambda"), // Code loaded from the "lambda" directory
-                Handler = "hello.handler" // file is "hello", function is "handler"
+                Handler = "hello.handler", // file is "hello", function is "handler"
+                Environment = new Dictionary<string, string>
+                {
+                    { "TABLE_NAME", table.TableName }
+                }
             });
+            table.GrantReadWriteData(hello);
 
             hello.AddEventSource(new SqsEventSource(props.inputQueue));
 
